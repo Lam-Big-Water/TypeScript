9d4883250c9d383917e2d17752def380b877fe8b
Sam Lam <xlin67948gmail.com>
2024-09-01 13:40:24 +0800

    the 5-6-7 question is recap

--- leetCodeJs/index.ts
+++ leetCodeJs/index.ts
@@ -45,23 +45,73 @@
 // expect(5).toBe(5); // true
 // expect(5).notToBe(5); // throws "Equal"
 
-// 4. 
-const createCounter2 = (init: number) => {
-    let count = init;
-    return {
-        increment() {
-            return ++count
-        },
-        reset() {
-            return count = init
-        },
-        decrement() {
-            return ++count
-        }
+// // 4. 
+// const createCounter2 = (init: number) => {
+//     let count = init;
+//     return {
+//         increment() {
+//             return ++count
+//         },
+//         reset() {
+//             return count = init
+//         },
+//         decrement() {
+//             return ++count
+//         }
+//     }
+// }
+
+// const counter2 = createCounter2(5);
+// console.log(counter2.increment());
+// console.log(counter2.reset());
+// console.log(counter2.decrement());
+
+// 5.
+function myMap<T, U>(arr: T[], fn: (value: T, index: number) => U): U[] {
+    const mappedArray: U[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        mappedArray.push(fn(arr[i], i))
     }
+
+    return mappedArray;
 }
 
-const counter2 = createCounter2(5);
-console.log(counter2.increment());
-console.log(counter2.reset());
-console.log(counter2.decrement());
+
+const newArr = myMap([1,2,3], function(v, i) {
+    return v + i
+});
+console.log(newArr);
+
+// 6.
+
+function myFilter<T,>(arr: T[], fn: (v: T, i?: number) => boolean): T[] {
+    const result: T[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+const newArr_2 = myFilter([1, 2, 3], function(n, i) {
+    return n > 1 && i === 1
+})
+
+console.log(newArr_2)
+
+// 7.
+function myReduce<T, U>(arr: U[], fn: (acc: T, curr: U) => T, init: T ): T {
+    let result: T = init;
+    for (let i = 0; i < arr.length; i++) {
+        result = fn(result, arr[i]);
+    }
+    return result;
+}
+
+const newArr_3 = myReduce([1, 2, 3], function(acc, curr) {
+    return acc + curr
+}, 0)
+
+console.log(newArr_3)
