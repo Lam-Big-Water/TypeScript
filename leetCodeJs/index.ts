8a9f955147e4942c0aa061eeb2732082db5f8f75
Sam Lam <xlin67948gmail.com>
2024-09-19 20:29:07 +0800

    13,14

--- leetCodeJs/index.ts
+++ leetCodeJs/index.ts
@@ -140,40 +140,152 @@
 
 // console.log(argumentsLength(1, 'c', true));
 
-// Allow One Function Call
 
-type Func<T, U> = (...args: T[]) => U;
 
-function once<T, U>(fn: Func<T, U>): Func<T, U | undefined> {
-    let hasBeenCalled = false;
-    let result: U;
+// 10. Allow One Function Call
 
-    return function (...args: T[]): U | undefined {
-        if (!hasBeenCalled) {
-            result = fn(...args);
-            hasBeenCalled = true;
-            return result;
-        } else {
-            return undefined;
-        }
-    }
-}
-const fn = (a: number, b: number, c: number) => (a + b +c);
-const onceFn = once(fn);
+// type Func<T, U> = (...args: T[]) => U;
 
-console.log(onceFn(1, 2, 3));
-console.log(onceFn(3, 2, 1));
+// function once<T, U>(fn: Func<T, U>): Func<T, U | undefined> {
+//     let hasBeenCalled = false;
+//     let result: U;
+
+//     return function (...args: T[]): U | undefined {
+//         if (!hasBeenCalled) {
+//             result = fn(...args);
+//             hasBeenCalled = true;
+//             return result;
+//         } else {
+//             return undefined;
+//         }
+//     }
+// }
+// const fn = (a: number, b: number, c: number) => (a + b +c);
+// const onceFn = once(fn);
+
+// console.log(onceFn(1, 2, 3));
+// console.log(onceFn(3, 2, 1));
 
 // my example
 
-const move = (action) => {
+// const move = (action) => {
 
-    return function (...steps) {
-       return action(...steps)
-    }
+//     return function (...steps) {
+//        return action(...steps)
+//     }
 
-}
+// }
 
-const action = (step, step2) => step + step2;
-const myMove = move(action);
-console.log(myMove(1, 2))
+// const action = (step, step2) => step + step2;
+// const myMove = move(action);
+// console.log(myMove(1, 2))
+
+
+// 11. Memoize
+// function memoize(fn) {
+//     const cache = {};
+//     return function memoized(...args) {
+//       const key = JSON.stringify(args);
+//       if (key in cache) {
+//         return cache[key];
+//       }
+//       const result = fn(...args);
+//       cache[key] = result;
+//       return result;
+//     }
+//   }
+
+// type Args = any[];
+// type MemoizedFn<T> = (...args: Args) => T;
+
+// function memoize<T>(fn: MemoizedFn<T>): MemoizedFn<T> {
+//     const cache: Record<string, T> = {};
+//     return function (...args: Args) {
+//         const key = JSON.stringify(args);
+//         console.log(key);
+
+//         if (key in cache) {
+//             return cache[key];
+//         }
+//         const result = fn(...args);
+//         cache[key] = result;
+//         return result;
+//     }
+// }
+// const sum = (a: number, b: number) => a + b;
+// const memoizedSum = memoize(sum);
+
+// console.log(memoizedSum(1,2))
+
+
+// 12. Add Two Promise
+// type PromiseType = Promise<number>;
+// const promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20));
+// const promise2 = new Promise(resolve => setTimeout(() => resolve(5), 50));
+
+// const receivePromises = (...args: PromiseType[]) => {
+//     Promise.all<PromiseType[]>([...args])
+//     .then(res => res.reduce((prev, curr) => prev + curr))
+//     .then(sum => console.log(sum))
+// }
+
+// receivePromises(
+// promise1,
+// promise2
+// )
+
+// 13. Sleep
+// const sleep = (sec: number) => {
+//     let t = Date.now();
+//     return new Promise(function (resolve, reject) {
+//         setTimeout(function () {
+//             resolve(console.log(Date.now() - t));
+//         }, sec * 1000);
+//     });
+// };
+// sleep(2);
+
+// 14. Timeout Cancellation
+type CancelType = () => any;
+// const cancellable = (fn: Function, args: any[], t: number): Function => {
+//     let willFnCall = true;
+//     setTimeout(() => willFnCall && console.log(fn(...args)), t);
+//     return () => willFnCall = false;
+// };
+
+// const fn = (x: number) => x * 5;
+
+// const args = [2];
+
+// const t = 20;
+
+// const cancelT = 21;
+
+// const start = <CancelType>cancellable(fn, args, t);
+
+// setTimeout(start, cancelT);
+
+
+const cancellable = function(fn: Function, args: any[], t: number) {
+    // cancelFn function//
+    const cancelFn = function (){
+      clearTimeout(timer);
+      console.log('pause')
+  };
+  const timer = setTimeout(()=>{
+      console.log(fn(...args), 'finished')
+  }, t);
+  
+  return cancelFn ;
+};
+
+const fn = (x: number) => x * 5;
+
+const args = [2];
+
+const t = 20;
+
+const cancelT = 21;
+
+const start = <CancelType>cancellable(fn, args, t);
+setTimeout(start, cancelT);
