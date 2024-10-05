eced817b6f64beb1fc34e2f3f0fff4ed5770c3dc
Sam Lam <xlin67948gmail.com>
2024-10-05 15:07:28 +0800

    the fundamentals part 1 is done

---
+++ Jonass-JS/01-Fundamentals.ts
@@ -0,0 +1,26 @@
+// Challenge #1
+// const marks = {
+//     weights: 78,
+//     tall: 1.69,
+// }
+
+// const john = {
+//     weights: 92,
+//     tall: 1.95,
+// }
+
+// type DataType = {
+//     weights: number;
+//     tall: number;
+// }
+
+// const bmi = ({weights, tall}: DataType) => {
+//     return weights / tall ** 2
+// }
+
+// let marksBMI = bmi(marks);
+// let johnBMI = bmi(john);
+// let compare = marksBMI > johnBMI ? 'marksBMI higher than johnBMI' : 'johnBMI higher than marksBMI'
+
+// console.log(marksBMI, johnBMI, compare);
+
